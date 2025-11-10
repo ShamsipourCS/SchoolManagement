9b522555cf2ec9d3c37abf609b1f8e775b22e567
Amirmahdi <amirmahdialipor972@gmail.com>
2025-11-10 20:13:36 +0330

    feat(domain): add IUnitOfWork interface

---
+++ SchoolManagement.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Domain.Interfaces;
+
+/// <summary>Unit of Work interface</summary>
+public interface IUnitOfWork : IDisposable
+{
+    IStudentRepository Students { get; }
+    ITeacherRepository Teachers { get; }
+    ICourseRepository Courses { get; }
+    IEnrollmentRepository Enrollments { get; }
+    Task<int> SaveChangesAsync();
+}
