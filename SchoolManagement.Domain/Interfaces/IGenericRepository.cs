32e22c5e231c8e50c93156e66fa80a4040143189
Amirmahdi <amirmahdialipor972@gmail.com>
2025-11-10 20:00:00 +0330

    feat(domain): add IGenericRepository interface

---
+++ SchoolManagement.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SchoolManagement.Domain.Entities;
+
+namespace SchoolManagement.Domain.Interfaces;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<bool> ExistsAsync(int id);
+}
