cd570f1ab198f3ef4500d27368cdbd271fa4a9d7
Amirmahdi <amirmahdialipor972@gmail.com>
2025-11-10 19:47:34 +0330

    feat(domain): add Course entity with enrollments

---
+++ SchoolManagement.Domain/Entities/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Domain.Entities;
+
+public class Course : BaseEntity
+{
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public DateTime StartDate { get; set; }
+
+    public int TeacherId { get; set; }
+    public virtual Teacher Teacher { get; set; } = null!;
+    public virtual ICollection<Enrollment> Enrollments { get; set; } = new List<Enrollment>();
+}
