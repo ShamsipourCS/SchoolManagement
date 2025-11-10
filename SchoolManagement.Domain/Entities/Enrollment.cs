da95fc6241f5abb5a8c5396e57cd34d98ed49945
Amirmahdi <amirmahdialipor972@gmail.com>
2025-11-10 19:45:26 +0330

    feat(domain): add Enrollment entity as join table

---
+++ SchoolManagement.Domain/Entities/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Domain.Entities;
+
+public class Enrollment : BaseEntity
+{
+    public DateTime EnrollDate { get; set; } = DateTime.UtcNow;
+    public decimal? Grade { get; set; }
+
+    public int StudentId { get; set; }
+    public virtual Student Student { get; set; } = null!;
+
+    public int CourseId { get; set; }
+    public virtual Course Course { get; set; } = null!;
+}
